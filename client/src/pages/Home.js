0966d4195f4709e394f75631f2ce85355d0a4640
Brandy Moore <brandymoore923@gmail.com>
2022-10-27 10:06:38 -0500

    added 4th image to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,6 +10,7 @@ import UserList from "../components/UserList";
 import marketing from "../assets/images/Marketing.png";
 import sales from "../assets/images/Sales.png";
 import time from "../assets/images/Time.png";
+import schedule from "../assets/images/Schedule.png";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_USERS);
@@ -30,23 +31,42 @@ const Home = () => {
 
   return (
     <main>
-      <div className="images">
-        <img width='250'
-          alt="marketing"
-          style={{ marginRight: "20px"}}
-          src={marketing}
-        ></img>
-        <img width='250'
-          alt="sales"
-          style={{ marginRight: "20px" }}
-          src={sales}
-          ></img>
-        <img width='250'
-          alt="time"
-          style={{ marginRight: "20px" }}
-          src={time}
-          ></img>
-      </div>
+      <section className="col-12 col-md-4 d-flex flex-column m-3">
+        <div>
+          <div>
+            <img
+              width="250"
+              alt="marketing"
+              style={{ marginRight: "20px" }}
+              src={marketing}
+            />
+          </div>
+          <div>
+            <img
+              width="250"
+              alt="sales"
+              style={{ marginRight: "20px" }}
+              src={sales}
+            />
+            </div>
+            <div>
+            <img
+              width="250"
+              alt="time"
+              style={{ marginRight: "20px" }}
+              src={time}
+            />
+            </div>
+            <div>
+            <img
+              width="250"
+              alt="time"
+              style={{ marginRight: "20px" }}
+              src={schedule}
+            />
+          </div>
+        </div>
+      </section>
       <div>{renderUsername()}</div>
       <div>{renderUserList()}</div>
     </main>
