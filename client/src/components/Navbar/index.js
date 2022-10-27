c15cc990e0727eb56998009ca45360eb367eb55f
Brandy Moore <brandymoore923@gmail.com>
2022-10-27 08:49:06 -0500

    trying to center nav text & fixed logo

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -8,10 +8,10 @@ function CollapsibleExample() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#home">
-          <img alt='logo'  width='200' src={Logo}/>
+          <img alt='logo' width='150' src={Logo}/>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav" >
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#sales">SALES.</Nav.Link>
             <Nav.Link href="#marketing">MARKETING.</Nav.Link>
