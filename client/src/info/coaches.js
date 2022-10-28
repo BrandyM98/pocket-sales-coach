8a0e4d890f14a5d9f267355c91d24acd22db4054
dds git <deeann@stelzl.us>
2022-10-28 16:49:30 -0400

    added form to Coach page

--- client/src/info/coaches.js
+++ client/src/info/coaches.js
@@ -4,13 +4,13 @@ import Coach3 from '../assets/images/Coach3.jpg';
 import Coach4 from '../assets/images/Coach4.png';
 
 const coaches = [
-  {name: "Coach Two", specialty: "Digital Marketing", calender: "https://calendly.com/pocket-coach-project/30min", desc: "After working with some of the world's top marketers, Coach One has successfully launched dozens of startups using digital marketing.", image: Coach2},
+  {name: "Wendy Clark", specialty: "Digital Marketing", calender: "https://calendly.com/pocket-coach-project/30min", desc: "After working with some of the world's top marketers, Wendy has successfully launched dozens of startups using digital marketing. Meet with her about social, email or sales funnels.", image: Coach2},
 
-  {name: "Coach One", specialty: "Sales", calender: "https://calendly.com/pocket-coach-project/30min", desc: "Coach One has been selling SaaS successfully for 20 years. For the last 5 years he has been training entreprenuers to find and execute leads - with conversion rates over 90%... Coach One is worth a call.", image: Coach1},
+  {name: "John Taylor", specialty: "Sales", calender: "https://calendly.com/pocket-coach-project/30min", desc: "John Taylor has been selling SaaS successfully for 20 years. For the last 5 years he has been training entreprenuers to find and execute leads - with conversion rates over 90%.", image: Coach1},
 
-  {name: "Coach Three", specialty: "Time Management", calender: "https://calendly.com/pocket-coach-project/30min", desc: "A huge subscriber to the world of Stephen Covey - Coach Three has successfully helped dozens of executives prioritize their days to maximize time for sales.", image: Coach3},
+  {name: "David Allen", specialty: "Time Management", calender: "https://calendly.com/pocket-coach-project/30min", desc: "A huge subscriber to the world of Stephen Covey - David Allen has successfully helped dozens of executives prioritize their days to maximize time for sales.", image: Coach3},
 
-  {name: "Coach Four", specialty: "Life Coach", calender: "https://calendly.com/pocket-coach-project/30min", desc: "Not sure where to start? Coach Four can help you with your mindset and get you ready for the next steps ahead.", image: Coach4}
+  {name: "Alison Canavan", specialty: "Life Coach", calender: "https://calendly.com/pocket-coach-project/30min", desc: "Not sure where to start? Alison has helped countless numbers of business executives find their way and can help you with your mindset as you build your business.", image: Coach4}
 ]
 
   export default coaches;
