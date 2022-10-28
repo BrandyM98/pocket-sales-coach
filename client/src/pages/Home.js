222b90712081586bc3c8512f5f65f58759f9d6d8
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-10-28 13:48:00 -0400

    Fixed Quote and Resized Images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,18 +7,14 @@ import Auth from "../utils/auth";
 import { QUERY_USERS } from "../utils/queries";
 // Components
 
-
 import marketing from "../assets/images/MKT.png";
 import sales from "../assets/images/SLS.png";
 import time from "../assets/images/TMG.png";
 import schedule from "../assets/images/SCHED.png";
 
-
 const Home = () => {
   const { data } = useQuery(QUERY_USERS);
   const user = data?.users || [];
-
-
 
   const renderUsername = () => {
     if (!Auth.loggedIn()) return null;
@@ -27,23 +23,19 @@ const Home = () => {
 
   return (
     <main className="homepage">
-
-           <div>
-            <h2 className="ps-5 pt-4"> Welcome {renderUsername()}  </h2>
-            </div>
-      <section className="images align-items-center">
-
-      <p className="pt-5 mt-5 text-center">
+      <div>
+        <h2 className="ps-5 pt-4">{renderUsername()} </h2>
+      </div>
+      <p className="text-center pt-5" style={{ paddingRight: "20%", paddingLeft: "20%" }} >
         "People who succeed have momentum. The more they succeed, the more they
         want to succeed, and the more they find a way to succeed. Similarly,
         when someone is failing, the tendency is to get on a downward spiral
         that can even become a self-fulfilling prophecy." -- Tony Robbins
       </p>
       <section className="images align-items-center pt-5">
-
         <Link to="/course/635a6f921c253cacc94f1d4c">
           <img
-            width="300"
+            width="200"
             alt="marketing"
             style={{ marginRight: "20px" }}
             src={marketing}
@@ -51,7 +43,7 @@ const Home = () => {
         </Link>
         <Link to="/course/635a6f921c253cacc94f1d4d">
           <img
-            width="300"
+            width="200"
             alt="sales"
             style={{ marginRight: "20px" }}
             src={sales}
@@ -59,21 +51,19 @@ const Home = () => {
         </Link>
         <Link to="/course/635a6f921c253cacc94f1d4e">
           <img
-            width="300"
+            width="200"
             alt="time"
             style={{ marginRight: "20px" }}
             src={time}
           />
         </Link>
         <img
-          width="300"
+          width="200"
           alt="time"
           style={{ marginRight: "20px" }}
           src={schedule}
         />
       </section>
-
-  
     </main>
   );
 };
